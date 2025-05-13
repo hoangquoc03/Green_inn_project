df50de3dc7c9c6ac08ab07358c43c164927d0179
hoangquoc03 <h.oangquoc180305@gmail.com>
2025-05-13 06:07:03 +0000

    CHU Y KY TU LA DAN DEN KHONG IMPORT DUOC PATH

---
+++ greenInn/src/App.java
@@ -0,0 +1,8 @@
+import GREENINN.test.TestGreenInn;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        TestGreenInn g = new TestGreenInn("abc", "abc", "abc");
+        System.out.print(g.Room);
+    }
+}
---
+++ greenInn/test/TestGreenInn.java
@@ -0,0 +1,14 @@
+
+
+//CHU Y KY TU LA O DAY
+public class TestGreenInn{
+    String Room;
+    String Tenant;
+    String Landlord;
+
+    public TestGreenInn(String Room, String Tenant,String Landlord){
+        this.Room = Room;
+        this.Tenant = Tenant;
+        this.Landlord =Landlord;
+    }
+}
